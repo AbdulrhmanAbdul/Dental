ccea697dc685b8bbd2143e4d8cad9c3f0ea56536
abdulrhman <109494338+AbdulrhmanAbdul@users.noreply.github.com>
2023-12-22 21:54:36 +0200

    Add files via upload

---
+++ java.js
@@ -0,0 +1,11 @@
+// let icon = document.querySelector(".icon");
+// let nav = document.querySelector("links-container");
+
+// function switching(){
+//     console.log(nav);
+//     nav.classList.toggle("boom");
+// }
+// icon.addEventListener("click",switching);
+
+// console.log(1+1);
+
